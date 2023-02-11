e6ade9adb0d5689abaa084b4700bbaab1e6ab90c
rmkofficial <kmert0@outlook.com>
2023-02-11 11:42:23 +0300

    dom to jquery

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-const panels = document.querySelectorAll(".panel");
+// const panels = document.querySelectorAll(".panel");
+const panels = $(".panel");
 
-panels.forEach((panel) => {
-  panel.addEventListener("click", () => {
-    removeActiveClasses();
-    panel.classList.add("active");
-  });
+$(".panel").click(function () {
+  $(".panel").not(this).removeClass("active");
+  $(this).toggleClass("active");
 });
 
-function removeActiveClasses() {
-  panels.forEach((panel) => {
-    panel.classList.remove("active");
-  });
-}
+// $(".panel").click(function () {
+//   $(this).removeClass("active");
+// });
+
+// panels.forEach((panel) => {
+//   panel.addEventListener("click", () => {
+//     removeActiveClasses();
+//     panel.classList.add("active");
+//   });
+// });
+
+// function removeActiveClasses() {
+//   panels.forEach((panel) => {
+//     panel.classList.remove("active");
+//   });
+// }
